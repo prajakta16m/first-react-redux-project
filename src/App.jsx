ecea2378a42bacd43368c9f4a9cf959fe061d4f5
Prajakta Hali <prajakta.mahamuni91@gmail.com>
2024-05-30 15:21:39 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import PButton from "./PButton";
+
+function App(){
+
+  return <div>
+  <h1>
+      Hello World
+    </h1>
+    <PButton></PButton>
+  </div>
+  
+  
+}
+
+export default App;
---
+++ src/PButton.jsx
@@ -0,0 +1,5 @@
+function PButton(){
+  return <button>Click here</button>
+}
+
+export default PButton;
